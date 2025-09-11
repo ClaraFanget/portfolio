854e85cf57c5b8bd596bd0b073e4dfff33f5760c
Clara <cfanget@ensc.fr>
2025-09-11 22:12:06 +0900

    [INIT] - Initialisation du projet

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+//import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        Voici mon portfolio
+      </div>
+      
+    </>
+  )
+}
+
+export default App
